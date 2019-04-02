0573dff3d919d3693b7800c877cae4b59efb3d28
Kevin <kevin.s.schwenk@gmail.com>
2019-04-02 22:42:24 +0000

    Removed snarky message

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import random
 import os
 from discord.utils import get
 
-#Did you really think you could get my token that easily?
 TOKEN = os.environ.get('DISCORDTOKEN', 'default value')
 
 client = discord.Client()
